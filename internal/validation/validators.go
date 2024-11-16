2cb457af0eef6a804cfaa3586dc92ca12bdc935d
Kranti <darkphoton20@gmail.com>
2024-11-16 21:24:27 +0800

    refactor: Restructuring validation placement.

--- internal/validation/validators.go
+++ internal/validation/validators.go
@@ -1,20 +1,21 @@
 package validation
 
 import (
+	"github.com/darkphotonKN/community-builds/internal/item"
 	"github.com/go-playground/validator/v10"
 )
 
 // RegisterValidators registers custom validators for categories, classes, and types
 func RegisterValidators(v *validator.Validate) {
 	v.RegisterValidation("category", func(fl validator.FieldLevel) bool {
-		return IsValidCategory(fl.Field().String())
+		return item.IsValidCategory(fl.Field().String())
 	})
 
 	v.RegisterValidation("class", func(fl validator.FieldLevel) bool {
-		return IsValidClass(fl.Field().String())
+		return item.IsValidClass(fl.Field().String())
 	})
 
 	v.RegisterValidation("type", func(fl validator.FieldLevel) bool {
-		return IsValidType(fl.Field().String())
+		return item.IsValidType(fl.Field().String())
 	})
 }
