71568dd5d0009f96acb2043a1680ecb995fbea37
Kranti <darkphoton20@gmail.com>
2024-11-28 00:01:19 +0800

    feat: Completed dbutils transaction helper, ready to refactor transaction code to use helper.

--- internal/utils/dbutils/helpers.go
+++ internal/utils/dbutils/helpers.go
@@ -14,26 +14,40 @@ import (
 * Accepts a function that expects a transaction argument and helps wrap the function call
 * with the initiation, passing, and error checking of that transaction.
 **/
-func execTx(db *sqlx.DB, fn func(tx *sqlx.Tx) error) error {
-	tx, err := db.Beginx()
+func execTx(db *sqlx.DB, fn func(tx *sqlx.Tx) error) (err error) {
+	tx, txBeginErr := db.Beginx() // NOTE: use "beginx" for the **sqlx** version
 
-	if err != nil {
-		fmt.Println("Error when attempting to start transaction:", err)
-		return fmt.Errorf("Error when attempting to start transaction: %s", err)
+	if txBeginErr != nil {
+		fmt.Printf("Error when attempting to start transaction: %v\n", txBeginErr)
+		return fmt.Errorf("Error when attempting to start transaction: %v", txBeginErr)
 	}
+
+	// handles panics and errors and rollback if they occur
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+
+			fmt.Println("Transaction rolled back due to panic:", p)
+
+			// re-throw panic after rollback
+			panic(p)
+		}
+
+		if err != nil {
+			fmt.Printf("Error during transaction, rolling back: Error:%v\n", err)
+			tx.Rollback()
+		}
+	}()
 
 	// call the function passed in and provide the transaction to it
 	err = fn(tx)
 
-	// function errored, rollback
-	if err != nil {
-		tx.Rollback()
-	}
-
 	// no error, safe to commit
-	if err = tx.Commit(); err != nil {
+	if commitErr := tx.Commit(); commitErr != nil {
+		fmt.Printf("failed to commit transaction, rolling back. Error: %s\n", commitErr)
 		tx.Rollback() // rollback if commit fails
-		return err
+
+		return commitErr
 	}
 
 	return nil
