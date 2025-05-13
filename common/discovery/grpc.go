32ef2d5849c11ceb389be5606dd71ef461b69968
Kranti <darkphoton20@gmail.com>
2025-05-13 21:01:00 +0800

    feat: updating proto file structuring for common

---
+++ common/discovery/grpc.go
@@ -0,0 +1,28 @@
+package discovery
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func ServiceConnection(ctx context.Context, serviceName string, registry Registry) (*grpc.ClientConn, error) {
+
+	// discover the other services
+	addrs, err := registry.Discover(ctx, serviceName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	length := len(addrs)
+
+	if length == 0 {
+		return nil, errors.New("There are no services to discover now.")
+	}
+
+	return grpc.Dial(addrs[rand.Intn(length)], grpc.WithTransportCredentials(insecure.NewCredentials()))
+}
