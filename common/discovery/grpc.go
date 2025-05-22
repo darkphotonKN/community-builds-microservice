8a19b191e5c82ac44f28a52b77802b711140269b
Nick Peng <gibson7780@gmail.com>
2025-05-23 00:52:38 +0800

    crawling item data

--- common/discovery/grpc.go
+++ common/discovery/grpc.go
@@ -3,6 +3,7 @@ package discovery
 import (
 	"context"
 	"errors"
+	"fmt"
 	"math/rand"
 
 	"google.golang.org/grpc"
@@ -11,9 +12,11 @@ import (
 
 func ServiceConnection(ctx context.Context, serviceName string, registry Registry) (*grpc.ClientConn, error) {
 
+	fmt.Println("serviceName: ", serviceName)
 	// discover the other services
 	addrs, err := registry.Discover(ctx, serviceName)
 
+	fmt.Println("addrs: ", addrs)
 	if err != nil {
 		return nil, err
 	}
@@ -23,6 +26,7 @@ func ServiceConnection(ctx context.Context, serviceName string, registry Registr
 	if length == 0 {
 		return nil, errors.New("There are no services to discover now.")
 	}
+	// credentials := insecure.NewCredentials()
 
-	return grpc.Dial(addrs[rand.Intn(length)], grpc.WithTransportCredentials(insecure.NewCredentials()))
+	return grpc.NewClient(addrs[rand.Intn(length)], grpc.WithTransportCredentials(insecure.NewCredentials()))
 }
